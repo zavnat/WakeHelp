612c7a3d18f28284debee4976723f150ac7b2fe2
Nataly <safnat11@gmail.com>
2020-05-24 17:31:28 +0300

    work with local notification

--- WakeHelp/AppDelegate.swift
+++ WakeHelp/AppDelegate.swift
@@ -12,13 +12,24 @@ import UserNotifications
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
-  
-  
+  let notificationCenter = UNUserNotificationCenter.current()
+
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    
-    return true
+      
+      notificationCenter.delegate = self
+      
+      let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+      
+      notificationCenter.requestAuthorization(options: options) {
+          (didAllow, error) in
+          if !didAllow {
+              print("User has declined notifications")
+          }
+      }
+      
+      return true
   }
+  
 
   // MARK: UISceneSession Lifecycle
 
@@ -37,3 +48,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        if response.notification.request.identifier == "Local Notification" {
+            print("Handling notifications with the Local Notification Identifier")
+        }
+        
+        completionHandler()
+    }
+    
+    func scheduleNotification(notificationType: String) {
+        
+        let content = UNMutableNotificationContent() // Содержимое уведомления
+        let categoryIdentifire = "Delete Notification Type"
+        
+        content.title = notificationType
+      
+       
+        content.body = "This is example how to create " + notificationType
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        content.categoryIdentifier = categoryIdentifire
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let identifier = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+        
+    }
+}
--- WakeHelp/ViewController.swift
+++ WakeHelp/ViewController.swift
@@ -12,114 +12,73 @@ import UserNotifications
 class ViewController: UIViewController,UIPickerViewDelegate {
   
   
+
+ 
   
   @IBOutlet weak var prefferedWakePicker: UIDatePicker!
   @IBOutlet weak var wakePicker: UIDatePicker!
   
-//  var prefferedWakeTime = 0.0
-//  var wakeTime = 0.0
-//  var alarmTime = 0.0
-  let notificationCenter = UNUserNotificationCenter.current()
+  var appDelegate = UIApplication.shared.delegate as? AppDelegate
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
-    
-    let options: UNAuthorizationOptions = [.alert, .sound, .badge]
-    
-    notificationCenter.requestAuthorization(options: options) {
-      (didAllow, error) in
-      if !didAllow {
-        print("User has declined notifications")
-      }
-    }
-    
-    let localeID = Locale.preferredLanguages.first
-    prefferedWakePicker.locale = Locale(identifier: localeID!)
-    
-    
-  }
-  
-  func getMinutesFromPicker(){
-    
-    let date = prefferedWakePicker.date
-    let date2 = wakePicker.date
-    
-    let sec = Int64(date.timeIntervalSince1970)
-    let sec2 = Int64(date.timeIntervalSince1970)
-    
-    let secDifferent = (sec2 - sec) / 21
-    let result = sec2 - secDifferent
-    
-    
-    
-  
-    
-    let newDate = Date(timeIntervalSince1970: TimeInterval(result))
-    
-    scheduleNotification(notificationType: "", date: newDate)
 
-//
-//    let hour = calendar.component(.hour, from: prefferedWakePicker.date)
-//    print(hour)
-//    let minute = calendar.component(.minute, from: prefferedWakePicker.date)
-//    print(minute)
-//
-//    let hourTwo = calendar.component(.hour, from: wakePicker.date)
-//    print(hourTwo)
-//    let minuteTwo = calendar.component(.minute, from: wakePicker.date)
-//    print(minuteTwo)
-    
-//    prefferedWakeTime = Double(hour * 60) + Double(minute)
-//    wakeTime = Double(hourTwo * 60) + Double(minuteTwo)
-//    print(prefferedWakeTime)
-//    print(wakeTime)
-    
   }
   
-  @IBAction func startButtonPressed(_ sender: UIButton) {
+  
+  
+  func fetchNotification () {
     
-    
-    
-//    getMinutesFromPicker()
-//    countTime()
+//    let content = UNMutableNotificationContent()
+//    content.title = "Hey I am a notification"
+//    content.body = "Look at me"
+//
+//    let date = Date().addingTimeInterval(10)
+//    let dateComponents = Calendar.current.dateComponents([.hour, .minute], from: date)
+//    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+//
+//    let request = UNNotificationRequest(identifier: "Local Notification", content: content, trigger: trigger)
+//
+//    center.add(request) { (error) in
+//      if let error = error {
+//        print("Error \(error.localizedDescription)")
+//      }
+//    }
   }
   
-//  func countTime (){
-//    let minutePerDay = (wakeTime - prefferedWakeTime) / 21
-//    print(minutePerDay)
-//    alarmTime = wakeTime + minutePerDay
-//    print(alarmTime)
+  
+  
+//  func getMinutesFromPicker(){
 //
-//    let alarm = Double(alarmTime / 60)
-//    print(String(format: "%.3f", alarm))
+//    let date = prefferedWakePicker.date
+//    let date2 = wakePicker.date
+//
+//    let sec = Int64(date.timeIntervalSince1970)
+//    let sec2 = Int64(date.timeIntervalSince1970)
+//
+//    let secDifferent = (sec2 - sec) / 21
+//    let result = sec2 - secDifferent
+//
+//
+//
+//
+//
+//    let newDate = Date(timeIntervalSince1970: TimeInterval(result))
+//
+//
+//
 //  }
   
-  func scheduleNotification(notificationType: String, date: Date) {
+  @IBAction func startButtonPressed(_ sender: UIButton) {
+    print("button pressed")
+    appDelegate?.scheduleNotification(notificationType: "A")
     
-    let triggerTime = Calendar.current.dateComponents([.hour, .minute], from: date)
-    print("\(triggerTime.hour) \(triggerTime.minute)")
-    
-    
-    let content = UNMutableNotificationContent() // Содержимое уведомления
-    
-    content.title = notificationType
-    content.body = "This is example how to create " + "\(notificationType) Notifications"
-    content.sound = UNNotificationSound.default
-    content.badge = 1
-  
-    let trigger = UNCalendarNotificationTrigger(dateMatching: triggerTime, repeats: true)
-    
-    
-    let identifier = "Local Notification"
-    let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
 
-    notificationCenter.add(request) { (error) in
-      if let error = error {
-        print("Error \(error.localizedDescription)")
-      }
-    }
   }
+  
 }
+
+
+  
+
 
